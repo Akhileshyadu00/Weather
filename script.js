7ec5175ebb38bbe3327bd7b9c1b3096cd15a7026
Akhileshyadu00 <akhileshyadu001@gmail.com>
2025-02-13 14:59:23 +0530

    jsfile

---
+++ script.js
@@ -0,0 +1,25 @@
+const userLocation = document.getElementById("user-location");
+const converter = document.getElementById("converter");
+const weatherIcon = document.querySelector(".weather-icon");
+const temperature = document.querySelector(".temperature");
+const feelslike = document.querySelector(".feel-like");
+const description = document.querySelector(".description");
+const date = document.querySelector(".date");
+const city = document.querySelector(".city");
+
+const Hvalue = document.getElementById("Hvalue");
+const Wvalue = document.getElementById("Wvalue");
+const SRvalue = document.getElementById("SRvalue");
+const SSvalue = document.getElementById("SSvalue");
+const Cvalue = document.getElementById("Cvalue");
+const UVvalue = document.getElementById("UVvalue");
+const Pvalue = document.getElementById("Pvalue");
+
+const forecast = document.querySelector(".forecast");
+
+function findUserLocation () {
+    alert(1);
+}
+
+
+
